01c4f29e476e367153252b38d76508180245baa9
MarcBaeuerle <92479171+MarcBaeuerle@users.noreply.github.com>
2023-09-11 22:26:43 -0700

    ??

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/Schartly/client/',
+    base: '/Schartly/',
     plugins: [react()],
 })
