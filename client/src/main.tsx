e2377b066124e30fc9e3bbdbe9deeb1d815db66f
MarcBaeuerle <92479171+MarcBaeuerle@users.noreply.github.com>
2024-01-08 21:38:54 -0800

    Hashrouter test

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,11 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 import '../public/output.css'
 
 console.log("%cWelcome to %cSchartable", "", "color: green; font-weight: bolder", "");
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <BrowserRouter>
+    <Router>
         <App />
-    </BrowserRouter>
+    </Router>
 )
